a2dfe2e538cfb8558295fb96769026f2e0a84e10
yabani98 <habala70@gmail.com>
2022-09-25 19:15:43 +0200

    added components to ChatPage

--- client/src/components/ChatPage.js
+++ client/src/components/ChatPage.js
@@ -1,13 +1,39 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
+import UsersList from "./UsersList";
+import Board from "./Board";
+import RoomsList from "./RoomsList";
 import "../styles/ChatPage.css";
-const ChatPage = ({socket, userName }) => {
+const ChatPage = ({ socket, userName }) => {
   const [isPublic, setIsPublic] = useState(true);
   const [room, setRoom] = useState("Public");
-  const [isJoined,setIsJoined] = useState(true);
+  const [isJoined, setIsJoined] = useState(true);
+  useEffect(() => {
+    socket.emit("newUser", userName, room);
+  }, []);
 
-
-
-
+  return (
+    <>
+      <UsersList
+        socket={socket}
+        userName={userName}
+        setRoom={setRoom}
+        setIsPublic={setIsPublic}
+        setIsJoined={setIsJoined}
+      />
+      <Board
+        socket={socket}
+        room={room}
+        isPublic={isPublic}
+        isJoined={isJoined}
+      />
+      <RoomsList
+        socket={socket}
+        room={room}
+        setRoom={setRoom}
+        setIsPublic={setIsPublic}
+      />
+    </>
+  );
 };
 
 export default ChatPage;
